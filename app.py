508803ac6f968e44fd5a5c83173232b46fe1e533
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-05-22 20:51:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import streamlit-folium as sf
 
 st.title('Random Visual')
+x = 0
 with st.sidebar:
   x = st.slider('Pick a number', 0, 1000)
