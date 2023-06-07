aadd6ee66bc9f3bf435c0a3bd6aa76939fd23813
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:03:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ def initialize():
 
 if __name__ == "__main__":
   plusbutton = False
+  numberOfRV = 0
   initialize()
   with st.sidebar:
     plusbutton = st.button('추가')
+    if plusbutton == True:
+      numberOfRV += 1
+      st.slider(f'x{numberOfRV}:', 0, 1000)
+      plusbutton = False
   plusbutton
