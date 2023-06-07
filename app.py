fbda03d22a5aef29ccf3868825a83dad4c87aa7b
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 14:22:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
   t = st.text_input('데이터 이름')
   d = st.text_input('데이터')
   l = list(map(int, d.split()))
-  df[str(t)] = l
+  df[str(t)] = pd.DataFrame(l)
   st.dataframe(df)
   
   choose = st.radio('그래프 형태 고르기', ['line-chart', 'area-chart', 'bar-chart', 'altair-chart'])
