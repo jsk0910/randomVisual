a9104890a9da2559437d96c3cf54094a4cd734d3
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:56:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 df = pd.DataFrame()
 
 if __name__ == "__main__":
+  st.title('Random Visual')
   t = st.text_input('데이터 이름')
   d = st.text_input('데이터')
   l = list(map(int, d.split()))
