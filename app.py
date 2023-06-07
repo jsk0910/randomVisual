3a39f88f6d019f7ae8f09b162f91d82cbe6e24ea
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:32:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-def initialize():
-  st.title('Random Visual')
-
-dataSet = []
-  
 if __name__ == "__main__":
-  initialize()
-  while True:
-    with st.sidebar:
-      if st.button('추가'):
-      
-        t = st.text_input('데이터 이름')
-        value = st.text_area('데이터')
-        dataSet.append({t: value})
-    dataSet
+  t = st.text_input('데이터 이름')
+  
