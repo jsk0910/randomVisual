be907b9b70204bba2955329442e361b1540b9fff
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:34:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+data = []
+
 if __name__ == "__main__":
   t = st.text_input('데이터 이름')
-  t
-  data = st.text_input('데이터')
+  d = st.text_input('데이터')
+  data.append({t: d})
   data
