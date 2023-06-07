d98a7bbf2e25e44ec63282e537085b43ebc665d3
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:43:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-data = []
-
 df = pd.DataFrame()
 
 if __name__ == "__main__":
   t = st.text_input('데이터 이름')
   d = st.text_input('데이터')
   l = list(map(int, d.split()))
-  data.append({t: [l]})
-  df = pd.DataFrame(data)
+  df = pd.DataFrame(l, columns=[t])
   st.dataframe(df)
