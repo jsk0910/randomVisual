fc1c45931a9b7a69b4bfef50849df59e5c805ea2
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:00:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+def initialize():
+  st.title('Random Visual')
+  with st.sidebar:
+    plusbutton = st.button('추가')
+
+if __name__ == "__main__":
+  plusbutton = False
+  initialize()
