9115cc2af0eef9770852d7a5bd4e134e0ac85a62
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 14:29:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ df = pd.DataFrame()
 if __name__ == "__main__":
   st.title('Random Visual')
   t = st.text_input('데이터 이름')
+  t1 = t.split(',')
   d = st.text_input('데이터')
-  l = list(map(int, d.split()))
-  df[str(t)] = l
+  d1 = d.split(',')
+  for i in d1:
+    l = list(map(int, i.split()))
+    df[str(t[i])] = l
   st.dataframe(df)
   
-  choose = st.radio('그래프 형태 고르기', ['line-chart', 'area-chart', 'bar-chart', 'altair-chart'])
+  choose = st.radio('그래프 형태 고르기', ['line-chart', 'area-chart', 'bar-chart'])
   if choose == 'line-chart':
     st.line_chart(df)
   if choose == 'area-chart':
     st.area_chart(df)
   if choose == 'bar-chart':
     st.bar_chart(df)
-  if choose == 'altair-chart':
-    st.altair_chart(df)
