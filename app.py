cb49d17d1276e2b64f0468b7e213e24e99cf9760
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:39:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 data = []
 
+df = pd.DataFrame()
+
 if __name__ == "__main__":
   t = st.text_input('데이터 이름')
   d = st.text_input('데이터')
   l = list(map(int, d.split()))
   data.append({t: l})
-  data
+  df = pd.DataFrame(data)
