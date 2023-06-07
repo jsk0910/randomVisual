0ad1085f114653b4c01eed5ca81a6d515c7b02ce
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-06-07 13:09:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import pandas as pd
 def initialize():
   st.title('Random Visual')
 
+dataSet = []
+  
 if __name__ == "__main__":
   initialize()
   with st.sidebar:
     if st.button('추가'):
       t = st.text_input('데이터 이름')
       value = st.text_area('데이터')
+      dataSet.append({t: value})
+  dataSet
