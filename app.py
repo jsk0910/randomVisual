234ae561b3dd9f1114c275b1d0524aaba8b541a6
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-07 23:25:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,20 @@ def addr_to_lat_lon(addr):
   match_first = result['documents'][0]['address']
   return float(match_first['y']), float(match_first['x'])
 
-API_KEY = "a2f453d9ca3702e131a49655686b4dc8"
-center_xy = list(addr_to_lat_lon(address))
-m = folium.Map(location=center_xy, zoom_start=16)
-folium.Marker(center_xy, 
-              popup="회사명",
-              tooltip="회사명"
-              ).add_to(m)
+def main():
+  center_xy = list(addr_to_lat_lon(address))
+  m = folium.Map(location=center_xy, zoom_start=16)
+  folium.Marker(center_xy, 
+                popup="회사명",
+                tooltip="회사명"
+                ).add_to(m)
 
-st_folium(m, width=725, returned_objects=[])
+  st_folium(m, width=725, returned_objects=[])
+  
+  with st.sidebar:
+    st.button('직장 선택')
+    st.button('탭')
+
+if __name__ == "__main__":
+  API_KEY = "a2f453d9ca3702e131a49655686b4dc8"
+  main()
