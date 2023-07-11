cdb26b3355ab10ed8369ddfa86e396d9609856e0
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-11 18:58:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def calculate_distance(df, center_xy):
   distance_list = []
   for i in df['latlon']:
     if i != None:
-      i = list(i)
+      #i = list(i)
       st.write(i)
       st.write(center_xy)
       y = abs(float(center_xy[0]) - float(i[0])) * 111
