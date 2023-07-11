115b1c47ff2dd1f32e7763b4746b3e53f2a6d24e
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-11 19:32:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,11 @@ def calculate_distance(df, center_xy):
     if i != None or i != '':
       if type(i) == str:
         i = i[1:-1].split(', ')
-        st.write(i)
         y = abs(float(center_xy[0]) - float(i[0])) * 111
         x = (math.cos(float(center_xy[0])) * 6400 * 2 * 3.14 / 360) * abs(float(center_xy[1]) - float(i[1]))
         distance = math.sqrt(x*x + y*y)
         if distance <= 3.0:
-          df_distance = pd.concat([df_distance, df[df['latlon'] == (i)]])
+          df_distance = pd.concat([df_distance, df[df['latlon'] == ('(' + i[0] + ', ' + i[1] + ')')]])
           distance_list.append(distance)
 
   df_distance = df_distance.drop_duplicates()
