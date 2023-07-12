81b59ad5ec2c7d80aed399577162b7e54d6411d2
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-13 00:52:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def makeMap(address):
   makeMarker(m, df_subway_distance, 'green', 'train')
   makeMarker(m, df_bus_distance, 'green', 'bus')
   makeMarker(m, df_hospital_distance, 'white', 'plus')
-  makeMarker(m, df_museum_distance, 'blue', folium.features.CustomIcon('./images/building-columns', icon_size=(14, 14)))
+  makeMarker(m, df_museum_distance, 'blue', folium.features.CustomIcon('./images/building-columns.PNG', icon_size=(14, 14)))
   return m
 
 # Marker
