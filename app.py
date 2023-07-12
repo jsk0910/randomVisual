a85e6faf3b758aec0868fbbc00d864915170c2a6
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-13 01:39:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def makeMap(address):
                     len(df_museum_distance.loc[(df_museum_distance['distance'] > 1.0) & (df_museum_distance['distance'] <= 3.0)])]
 
   options = {
-    "title": {"show": True, "text": "500m"},
+    "title": {"show": True, "subtext": "500m", "subtextStyle": {"align": "center", }},
     "tooltip": {"trigger": "item"},
     "legend": {"top": "0%", "left": "center"},
     "series": [
