c321c031df97d9af41b6c5f2c8aa90c8926d69fe
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-13 01:06:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def makeMap(address):
 
   options = {
     "tooltip": {"trigger": "item"},
-    "legend": {"top": "10%", "left": "center"},
+    "legend": {"top": "-5%", "left": "center"},
     "label": "500m",
     "series": [
       {
