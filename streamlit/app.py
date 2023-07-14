664229e193ff78bef1ca1347afb3dc38db8f033a
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-14 17:49:48 +0900

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -188,7 +188,7 @@ def makeMap(address):
   makeMarker(m, df_subway_distance, 'orange', 'train')
   makeMarker(m, df_bus_distance, 'orange', 'bus')
   makeMarker(m, df_hospital_distance, 'red', 'plus')
-  makeMarker(m, df_museum_distance, 'blue', 'landmark')
+  makeMarker(m, df_museum_distance, 'blue', 'institution')
   makeMarker(m, df_starbucks_distance, 'green', 'coffee')
   return m
 
