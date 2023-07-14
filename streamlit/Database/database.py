9c16bfcaf71bdd7c9152d53e731459784fd16069
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-14 11:35:31 +0900

    Update database.py

--- streamlit/Database/database.py
+++ streamlit/Database/database.py
@@ -1,3 +1,8 @@
 import certifi
 from pymongo import MongoClient
 
+def connectDB(dbName):
+  client = MongoClient('mongodb+srv://wonseok:E3kXD7Tta02OWXYT@cluster0.0nbzrz6.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+  db = client.dbName
+  
+  return db
