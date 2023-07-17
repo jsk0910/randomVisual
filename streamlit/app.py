632580c5f0049c0b0936e7bc1216dbaa8ca1e0db
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-17 18:30:01 +0900

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -119,7 +119,7 @@ def makeMap(address):
           "borderColor": "#fff",
           "borderWidth": 2,
         },
-        "label": {"show": True, "formatter": ['500m'].join('\n'), "position": "bottom"},
+        "label": {"show": True, "formatter": ['500m'], "position": "bottom"},
         "emphasis": {
           "label": {"show": True, "fontSize": "20", "fontWeight": "bold"}
         },
